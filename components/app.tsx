5a9e2f21480a9fc340475f17b21a3cc68c320ff8
Kyle Henderson <ona@lunch.run>
2022-07-17 15:05:20 -0500

    format

--- components/app.tsx
+++ components/app.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
 import { FocusScope } from "@react-aria/focus"
-import Image from 'next/image'
+import Image from "next/image"
 
-type Edit = {
-  type: "style_transfer"
-  state: "not_started" | "loading" | "done" | "error"
-  result: string
-} | { type: 'none' }
+type Edit =
+  | {
+      type: "style_transfer"
+      state: "not_started" | "loading" | "done" | "error"
+      result: string
+    }
+  | { type: "none" }
 
 type Node = {
   source: string
@@ -20,27 +22,35 @@ type State = {
   children: Array<Node>
 }
 
-async function getImages () {
-  await new Promise(resolve => setTimeout(resolve, 500))
+async function getImages() {
+  await new Promise((resolve) => setTimeout(resolve, 500))
 
   return [
-    '/images/exploit_1.png',
-    '/images/exploit_2.png',
-    '/images/exploit_3.png',
-    '/images/exploit_4.png',
-    '/images/exploit_5.png',
-    '/images/exploit_6.png',
+    "/images/exploit_1.png",
+    "/images/exploit_2.png",
+    "/images/exploit_3.png",
+    "/images/exploit_4.png",
+    "/images/exploit_5.png",
+    "/images/exploit_6.png",
   ]
 }
 
-function Query ({ query }: { query: string }) {
-  return <h2 className="text-3xl mb-4"><span className="text-indigo-500">"</span>{query}<span className="text-indigo-500">"</span></h2>
+function Query({ query }: { query: string }) {
+  return (
+    <h2 className="text-3xl mb-4">
+      <span className="text-indigo-500">"</span>
+      {query}
+      <span className="text-indigo-500">"</span>
+    </h2>
+  )
 }
 
-function Container ({ children } : { children: React.ReactNode }) {
-  return <main className="py-16 w-full max-w-7xl mx-auto px-2 xm:px-6 lg:px-8 h-full ">
-    {children}
-  </main>
+function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="py-16 w-full max-w-7xl mx-auto px-2 xm:px-6 lg:px-8 h-full ">
+      {children}
+    </main>
+  )
 }
 
 export default function App() {
@@ -55,44 +65,56 @@ export default function App() {
       return (
         <Container>
           <FocusScope contain autoFocus restoreFocus>
-          <input
-            className="shadow-md hover:shadow-lg focus:shadow-lg w-full h-12 border-2 border-black focus:border-indigo-500 outline-none px-4 text-lg br-2"
-            onChange={(e) => setState({ ...state, query: e.target.value })}
-            value={state.query}
-          />
-          <button
-            type="submit"
-            className="br-2 h-12 bg-indigo-500 shadow-md hover:shadow-lg focus:shadow-lg text-white ml-4 px-4"
-            onClick={async () => {
-              setState({...state, status: 'loading' })
-              const images = await getImages()
-              const children = images.map(s => ({ source: s, edit: { type: 'none' as const }, children: [] }))
-              setState({ ...state, status: 'done', children })
-            }}
-          >
-            Generate
-          </button>
+            <input
+              className="shadow-md hover:shadow-lg focus:shadow-lg w-full h-12 border-2 border-black focus:border-indigo-500 outline-none px-4 text-lg br-2"
+              onChange={(e) => setState({ ...state, query: e.target.value })}
+              value={state.query}
+            />
+            <button
+              type="submit"
+              className="br-2 h-12 bg-indigo-500 shadow-md hover:shadow-lg focus:shadow-lg text-white ml-4 px-4"
+              onClick={async () => {
+                setState({ ...state, status: "loading" })
+                const images = await getImages()
+                const children = images.map((s) => ({
+                  source: s,
+                  edit: { type: "none" as const },
+                  children: [],
+                }))
+                setState({ ...state, status: "done", children })
+              }}
+            >
+              Generate
+            </button>
           </FocusScope>
         </Container>
       )
 
-    case 'loading':
-      return <Container>
+    case "loading":
+      return (
+        <Container>
           <Query query={state.query} />
-        <div>Loading...</div>
-      </Container>
+          <div>Loading...</div>
+        </Container>
+      )
 
-    case 'done':
-      return <Container>
-        <FocusScope autoFocus contain restoreFocus>
-          <Query query={state.query} />
-        <div className="grid grid-cols-3 gap-4">
-          {state.children.map(node => {
-            return <button className="relative h-48 border-2 border-black hover:border-indigo-500 focus:border-indigo-500 outline-none hover:shadow-md focus:shadow-md"><Image layout="fill" src={node.source} alt="image" /></button>
-          })}
-        </div>
-        </FocusScope>
-      </Container>
+    case "done":
+      return (
+        <Container>
+          <FocusScope autoFocus contain restoreFocus>
+            <Query query={state.query} />
+            <div className="grid grid-cols-3 gap-4">
+              {state.children.map((node) => {
+                return (
+                  <button className="relative h-48 border-2 border-black hover:border-indigo-500 focus:border-indigo-500 outline-none hover:shadow-md focus:shadow-md">
+                    <Image layout="fill" src={node.source} alt="image" />
+                  </button>
+                )
+              })}
+            </div>
+          </FocusScope>
+        </Container>
+      )
 
     default:
       return (
